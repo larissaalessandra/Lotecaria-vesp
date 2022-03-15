54491bb5efe7a07a157315f9640d436f4371cd16
matheusvmb <matheus.barreto2@estudante.ifms.edu.br>
2022-03-15 17:10:25 -0400

    Arrumei ai 100000% melhor

--- funcoes.js
+++ funcoes.js
@@ -1,23 +1,21 @@
-var numSort = []
-var numDig  = []
+var malaco = []
+var Baco  = []
 
-function sorteioNum(){
-    if(numDig.length < 6){
+function Tataco(){
+    if(Baco.length < 6){
         alert("Digita tudo ai pô")
     }else{
-    numSort=[]
+    malaco=[]
 
     do{
         let sort = Math.ceil(Math.random()*60)        
-        if(!numSort.includes(sort))
-            numSort.push(sort) 
+        if(!malaco.includes(sort))
+            malaco.push(sort) 
           
-    }while(numSort.length<6)
+    }while(malaco.length<6)       
 
-    console.log(numSort)         
-
-    document.getElementById("numsort").innerText = numSort
-    verificaAcertos()
+    document.getElementById("malaco").innerText = malaco
+    tico()
     }
 }
 
@@ -28,23 +26,23 @@ function  getNum(valor, pos){
        alert("O valor é maior que 60")
     }else if(valor <=0){
         alert("O valor não pode ser menor que 0")
-    }else if(numDig.includes(valor)) {
+    }else if(Baco.includes(valor)) {
         alert("O valor ja foi digitado!")
 
     }
     
     else{
-        numDig[pos]=valor
+        Baco[pos]=valor
     }
 }
 
-function verificaAcertos(){
-    let cont = 0
-    numDig.forEach(function (valor, index){
-        if(numSort.includes(valor)){
-            cont = cont+1
+function tico(){
+    let pato = 0
+    Baco.forEach(function (valor, index){
+        if(malaco.includes(valor)){
+            pato = pato+1
 
         }
     })
-    document.getElementById("total").innerText = cont
+    document.getElementById("teco").innerText = pato
 }
